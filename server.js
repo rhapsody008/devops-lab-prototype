181f0533855bc4542598a5ec147afd145c890fc0
Zhou Yi <yi.zhou@nutanix.com>
2025-12-10 15:50:26 +0800

    modify server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 // CHANGE THIS STRING to test your pipeline!
-const message = "Hello from Nutanix!"; 
+const message = "Hello from Nutanix! This is User02 speaking!"; 
 
 app.get('/', (req, res) => {
   res.send(message);
